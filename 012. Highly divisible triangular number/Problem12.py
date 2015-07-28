b94b162551552f3278cced0cc839479e0068dd55
Gene Der Su <e870252314@yahoo.com.tw>
2015-07-28 15:57:34 -0700

    Added solution for problem 12

--- 012. Highly divisible triangular number/Problem12.py
+++ 012. Highly divisible triangular number/Problem12.py
@@ -1,55 +1,48 @@
 ##Written by Gene Der Su at 7/28/2015
 ##Python verson 2.7.6
+##using divisor function to find number of divisor
+##source: https://en.wikipedia.org/wiki/Divisor_function
 
 import math
 import time
 
 initialTime = time.time()
 
-maxNumber=4
-with open('grid.txt','r') as i:
-    lines = i.readlines()
-
-matrix = []
-for line in lines:
-    matrix.append(line.split())
-
-maxProduct=0
-##checking from left to right
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])-maxNumber+1):
-        tmpProduct=1
-        for k in range(maxNumber):
-            tmpProduct*=int(matrix[i][j+k])
-        if (tmpProduct > maxProduct):
-            maxProduct = tmpProduct
-
-##checking from up to down
-for i in range(len(matrix)-maxNumber+1):
-    for j in range(len(matrix[i])):
-        tmpProduct=1
-        for k in range(maxNumber):
-            tmpProduct*=int(matrix[i+k][j])
-        if (tmpProduct > maxProduct):
-            maxProduct = tmpProduct
-
-##checking from up left to down right
-for i in range(len(matrix)-maxNumber+1):
-    for j in range(len(matrix[i])-maxNumber+1):
-        tmpProduct=1
-        for k in range(maxNumber):
-            tmpProduct*=int(matrix[i+k][j+k])
-        if (tmpProduct > maxProduct):
-            maxProduct = tmpProduct
-
-##checking from down left to up right
-for i in range(len(matrix)-maxNumber+1):
-    for j in range(maxNumber-1,len(matrix[i])):
-        tmpProduct=1
-        for k in range(maxNumber):
-            tmpProduct*=int(matrix[i+k][j-k])
-        if (tmpProduct > maxProduct):
-            maxProduct = tmpProduct
+def constructPrimeList(initial, final, primeList):
+    for i in range(initial, final):
+        primeList.append(True)
         
+    loopLimit=int(math.ceil(math.sqrt(final)))
+
+    for i in range(initial,loopLimit):
+        if primeList[i]:
+            for j in range(i**2,final,i):
+                primeList[j]=False
+    
+    return primeList
+
+def divisor(numberToCheck, primeList):
+    numberOfDivisor=1
+    for i in range(2,len(primeList)):
+        if primeList[i]&(numberToCheck%i==0):
+            counter=1
+            while (numberToCheck%i==0):
+                numberToCheck/=i
+                counter+=1
+            numberOfDivisor*=counter
+
+    return numberOfDivisor 
+
+minNumber=500
+numberOfDivisor=0
+currentCheck=1
+count=1
+primeList=[False, False]
+primeList = constructPrimeList(2, minNumber*2, primeList)
+while (numberOfDivisor < minNumber):
+    count+=1
+    currentCheck+=count
+    numberOfDivisor = divisor(currentCheck, primeList)
+    
 print time.time()-initialTime
-print maxProduct
+print currentCheck
