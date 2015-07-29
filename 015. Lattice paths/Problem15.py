c8451ade549d196a809156f4741fcf7a10eeabd9
Gene Der Su <e870252314@yahoo.com.tw>
2015-07-29 12:02:27 -0700

    Added solution for problem 15

---
+++ 015. Lattice paths/Problem15.py
@@ -0,0 +1,17 @@
+##Written by Gene Der Su at 7/28/2015
+##Python verson 2.7.6
+
+import time
+
+initialTime = time.time()
+
+maxNumber=20
+matrix=[[1]*maxNumber]*maxNumber
+
+for i in range(1,maxNumber):
+    for j in range(1,maxNumber):
+        matrix[i][j]=matrix[i-1][j]+matrix[i][j-1]
+
+numberOfPath=2*sum(matrix[maxNumber-1])
+print time.time()-initialTime
+print numberOfPath
